783efaff870ca4614a92387ea63c7a18ef781eab
Laura Queyla <laura.queyla@gmail.com>
2020-10-20 17:34:06 +0200

    Première version du panier #5

--- js/model/Cart.js
+++ js/model/Cart.js
@@ -1,21 +1,26 @@
 export default class Cart {
 
-    constructor() {
-      this.items = JSON.parse(localStorage.getItem('cart')) ||  [];
-    }
+  constructor() {
+    this.items = JSON.parse(localStorage.getItem('cart')) || [];
+  }
 
-    addItem(item,qty){  
+  addItem(item, qty) {
+    this.items.push(item, qty);
+    localStorage.setItem('cart', JSON.stringify(this.items));
+  }
 
-        this.items.push("toto");
-        localStorage.setItem('cart', JSON.stringify(this.items))
-    }
+  deleteItem(item, qty) {
+    this.items.remove(item, qty);
+    localStorage.deleteItem('cart', JSON.stringify(this.items));
+  }
 
-    deleteItem(item){   }
+  getItems(item, qty) {
+    this.items.get(item, qty);
+    localStorage.getItem('cart', JSON.stringify(this.items));
+  }
 
-    getItems(){}
 
-
-    clear(){
-
-    }
+  clear(item) {
+    localStorage.clear(item);
+  };
 }
